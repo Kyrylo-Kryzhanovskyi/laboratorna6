72ec0b4ac5e334cc6db769d2f1e62132551e8f2a
Kyrylo Kryzhanovskyi <kryzhanovskyi2021kb13@student.karazin.ua>
2024-11-14 13:16:48 +0200

    Task2

---
+++ src/Task2/Task2.java
@@ -0,0 +1,39 @@
+package Task2;
+
+import java.io.Console;
+import java.util.Arrays;
+import java.util.Scanner;
+// cd src
+// javac Task2/Task2.java
+// java Task2/Task2
+public class Task2 {
+
+    public static void main(String[] args) {
+        Console cons = System.console();
+        if (cons != null) {
+            String login = cons.readLine("Логін:");
+            char[] passwd = cons.readPassword("%s",
+                    "Пароль:");
+            System.out.printf("Хеш пароля: " + hashPassword(passwd));
+            System.out.printf("\nВаш логін: %s", login);
+            if (checkThePass(hashPassword(passwd))){
+                System.out.println("\nПароль правильний. Доступ надано\n");
+            }
+            else {
+                System.out.println("\nПароль не правильний. У доступі відмовлено\n");
+            }
+            Arrays.fill(passwd, ' ');
+        }
+    }
+    public static boolean checkThePass(int hash){
+        final int STORED_HASH = 43930;
+            return STORED_HASH==hash;
+        }
+    public static int hashPassword(char[] password) {
+        int hash = 0;
+        for (char c : password) {
+            hash = hash * 5 + c + 7;
+        }
+        return hash;
+    }
+}
